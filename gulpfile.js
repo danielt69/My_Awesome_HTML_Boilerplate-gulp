95dbf824f4d546b47f435cf934e5e5dff6504432
Daniel <danielt69@gmail.com>
2016-08-31 14:47:45 +0300

    gulpfile sourcemap fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 var gulp = require('gulp');
+// var debug = require('gulp-debug');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
-var plumber = require('gulp-plumber');
+// var plumber = require('gulp-plumber');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 var sass = require('gulp-sass');
-var csslint = require('gulp-csslint');
-var autoPrefixer = require('gulp-autoprefixer');
+// var csslint = require('gulp-csslint');
+// var autoPrefixer = require('gulp-autoprefixer');
 //if node version is lower than v.0.1.2
-require('es6-promise').polyfill();
+// require('es6-promise').polyfill();
 var cssComb = require('gulp-csscomb');
 var cmq = require('gulp-merge-media-queries');
 var cleanCss = require('gulp-clean-css');
@@ -19,74 +20,84 @@ var concat = require('gulp-concat');
 var minifyHtml = require('gulp-minify-html');
 var imageMin = require('gulp-imagemin');
 var cache = require('gulp-cache');
+var pxtorem = require('gulp-pxtorem');
+
 gulp.task('sass',function(){
     gulp.src(['src/css/main.scss'])
-        .pipe(plumber({
-            handleError: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        }))
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            includePaths: require('node-neat').includePaths
-        }))
-        .pipe(autoPrefixer())
+        // .pipe(plumber({
+        //     handleError: function (err) {
+        //         console.log(err);
+        //         this.emit('end');
+        //     }
+        // }))
+        // .pipe(sourcemaps.init({loadMaps: false}))
+        // .pipe(sass({
+        //     includePaths: require('node-neat').includePaths
+        // }))
+        .pipe(sass())
+        // .pipe(autoPrefixer())
         .pipe(cssComb())
         .pipe(cmq({log:true}))
-        .pipe(csslint())
-        .pipe(csslint.reporter())
+        // .pipe(csslint())
+        // .pipe(csslint.reporter())
         .pipe(concat('main.css'))
+        // .pipe(debug())
         .pipe(gulp.dest('dist/css'))
         .pipe(rename({
             suffix: '.min'
         }))
         .pipe(cleanCss())
+        .pipe(pxtorem({replace: false }))
         .pipe(sourcemaps.write())
+        // .pipe(debug())
         .pipe(gulp.dest('dist/css'))
         .pipe(reload({stream:true}))
 });
 gulp.task('js',function(){
     gulp.src(['src/js/**/*.js'])
-        .pipe(plumber({
-            handleError: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        }))
+        // .pipe(plumber({
+        //     handleError: function (err) {
+        //         console.log(err);
+        //         this.emit('end');
+        //     }
+        // }))
         .pipe(concat('main.js'))
         .pipe(jshint())
-          .pipe(jshint.reporter('default'))
+          // .pipe(jshint.reporter('default'))
           .pipe(browserify())
+          // .pipe(debug())
         .pipe(gulp.dest('dist/js'))
         .pipe(rename({
             suffix: '.min'
         }))
         .pipe(uglify())
+        // .pipe(debug())
         .pipe(gulp.dest('dist/js'))
         .pipe(reload({stream:true}))
 });
 gulp.task('html',function(){
     gulp.src(['src/*.html'])
-        .pipe(plumber({
-            handleError: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        }))
+        // .pipe(plumber({
+        //     handleError: function (err) {
+        //         console.log(err);
+        //         this.emit('end');
+        //     }
+        // }))
         .pipe(minifyHtml())
+        // .pipe(debug())
         .pipe(gulp.dest('dist/'))
         .pipe(reload({stream:true}))
 });
 gulp.task('image',function(){
     gulp.src(['src/images/**/*'])
-        .pipe(plumber({
-            handleError: function (err) {
-                console.log(err);
-                this.emit('end');
-            }
-        }))
+        // .pipe(plumber({
+        //     handleError: function (err) {
+        //         console.log(err);
+        //         this.emit('end');
+        //     }
+        // }))
         .pipe(cache(imageMin()))
+        // .pipe(debug())
         .pipe(gulp.dest('dist/images'))
         .pipe(reload({stream:true}))
 });
@@ -94,8 +105,9 @@ gulp.task('default',function(){
     browserSync.init({
         server: "./dist/"
     });
+    gulp.start('sass', 'js', 'html', 'image');
     gulp.watch('src/js/**/*.*',['js']);
     gulp.watch('src/css/**/*.*',['sass']);
     gulp.watch('src/*.html*',['html']);
-    gulp.watch('src/images/**/*',['image']);
+    gulp.watch('src/images/**/*.*',['image']);
 });
